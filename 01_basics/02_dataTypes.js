d51e47d3f6674b2c7e93a6e07703b3f895f074a6
shobhit0719 <shobhit9430@gmail.com>
2024-08-24 13:00:38 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat aal JS code as newer version
+
+// alert(3 + 3) // We are using nodejs, not browser so we can't acesss
+
+// Data Types:-
+// number
+// BigInt
+// String => " " or ' '
+// Boolean => true/false
+// null => standAlone value
+// undefined => 
+// Symbol => To find uniqueness
+
+
+console.log(typeof "Shobhit");//to know which type
+
+console.log(typeof undefined); // That's output come type undefined
+console.log(typeof null); // Thats's output come type object
+
+
