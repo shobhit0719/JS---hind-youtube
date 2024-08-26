58ef3acb7c3ff70007dc7d83c581ee1dde35d346
shobhit0719 <shobhit9430@gmail.com>
2024-08-26 09:41:35 +0000

    operator

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,41 @@
+// ****************OPERATIONS****************
+
+let value = 3
+let negValue = - value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "Shobhit"
+let str2 = " Rajput"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2 + 2);// In this treated all as string
+console.log(1 + 2+ "2");// In this firstly operration performed 
+
+console.log(true); // This prints True
+console.log(+true); // This prints 1 
+
+console.log(+"");//This prints 0
+
+
+
+
+
+
+
+
+
+
+
+
